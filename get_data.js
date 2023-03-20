211f89118767e76c52fb89c7c4e5f840a5a6afaa
Kévin C <kevin.chron@gmail.com>
2023-03-20 15:26:49 +0100

    API fetch et création style.css

--- get_data.js
+++ get_data.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+const getTodos = async (url) => {
+    const response = await fetch(url);
+
+    if (response.status != 200) {
+        throw new Error("Cannot fetch the data.");
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+getTodos("https://api.open-meteo.com/v1/meteofrance?latitude=47.22&longitude=-1.55&hourly=temperature_2m,precipitation,weathercode&daily=sunrise,sunset&timezone=Europe%2FBerlin")
+    .then(data => console.log("resolved\n", data))
+    .catch(err => console.log("rejected\n", err.message))

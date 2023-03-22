1e1c6ab04e1ef237df559695ceaff9f2582c777c
Kévin C <kevin.chron@gmail.com>
2023-03-22 09:41:38 +0100

    résol°  bug formatDateAndTime et début isItDay

--- get_data.js
+++ get_data.js
@@ -17,6 +17,7 @@ getWeather("https://api.open-meteo.com/v1/meteofrance?latitude=47.22&longitude=-
         const precipitation = data.hourly.precipitation[hourlyIndex]
         const weathercode = data.hourly.weathercode[hourlyIndex]
         console.log(weathercode, temperature, precipitation)
+        console.log(isItDay(sunrise(data), sunset(data)))
     })
     .catch(err => console.log("rejected\n", err.message))
     
@@ -76,6 +77,9 @@ function formatDateAndTime(dateAndTime=timer()) {
     let date = dateAndTime[0].split("/")
     let time = dateAndTime[1].split(":")
     time = time[0]
+    if (parseInt(time) < 10) {
+        time = "0" + time
+    }
     dateAndTime = date[2] + "-" + date[1] + "-" + date[0] + "T" + time +":00"
     return dateAndTime
 } 
@@ -87,6 +91,33 @@ function returnIndexOfDate(data, currentDateAndTime) {
     return data.hourly.time.indexOf(currentDateAndTime)
 }
 
+// Retourne l'heure de levé du soleil et l'heure de couché
+
+function sunrise(data) {
+    let temp = data.daily.sunrise[0].split("T")
+    temp = temp[1].split(":")
+    const hours = parseInt(temp[0])
+    const minutes = parseInt(temp[1])
+
+    return [hours, minutes]
+}
+
+function sunset(data) {
+    let temp = data.daily.sunset[0].split("T")
+    temp = temp[1].split(":")
+    const hours = parseInt(temp[0])
+    const minutes = parseInt(temp[1])
+
+    return [hours, minutes]
+}
+
+// Fonction qui retourne si il fait jour ou non
+
+function isItDay(sunrise=sunrise(data), sunset=sunset(data), time=timer()[1]) {
+    time = time.split(":")
+    
+    return time
+}
 
 
 
